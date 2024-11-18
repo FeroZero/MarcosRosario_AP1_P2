5505a354f60c139b0f05850ab7848d868d7e6b6e
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-18 02:50:12 -0500

    componentes y servicio.

--- Services/Service.cs
+++ Services/Service.cs
@@ -1,6 +1,52 @@
-﻿namespace MarcosRosario_AP1_P2.Services
+﻿using MarcosRosario_AP1_P2.DAL;
+using MarcosRosario_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MarcosRosario_AP1_P2.Services
 {
-	public class Service
+	public class Service(IDbContextFactory<Contexto> DbFactory)
 	{
+
+		private async Task<bool> Existe(int id)
+		{
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return true;
+        }
+
+		private async Task<bool> Insertar(Registro registro)
+		{
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return true;
+        }
+
+		private async Task<bool> Modificar(Registro registro)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return true;
+		}
+
+		public async Task<bool> Eliminar(int id)
+		{
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return true;
+        }
+
+		public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> registro)
+		{
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return null;
+        }
+
+
+		public async Task<Registro?> Buscar(int id)
+		{
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return null;
+        }
+		public async Task<bool> Guardar(Registro registro)
+		{
+			return true;
+		}
 	}
 }
