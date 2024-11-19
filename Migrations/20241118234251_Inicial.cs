1742faf00d5a3732a1cdf3d5905bfae3c00f7669
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-18 19:41:16 -0500

    Final del examen.

---
+++ Migrations/20241118234251_Inicial.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarcosRosario_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Combos",
+                columns: table => new
+                {
+                    ComboId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateOnly>(type: "date", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Vendido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Combos", x => x.ComboId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CombosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Costo = table.Column<int>(type: "int", nullable: false),
+                    ComboId = table.Column<int>(type: "int", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CombosDetalle", x => x.DetalleId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CombosDetalle",
+                columns: new[] { "DetalleId", "ArticuloId", "Cantidad", "ComboId", "Costo" },
+                values: new object[] { 1, 1, 10, 0, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Combos");
+
+            migrationBuilder.DropTable(
+                name: "CombosDetalle");
+        }
+    }
+}
--- Services/Service.cs
+++ Services/Service.cs
@@ -5,48 +5,60 @@ using System.Linq.Expressions;
 
 namespace MarcosRosario_AP1_P2.Services
 {
-	public class Service(IDbContextFactory<Contexto> DbFactory)
+	public class ComboService(IDbContextFactory<Contexto> DbFactory)
 	{
 
 		private async Task<bool> Existe(int id)
 		{
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return true;
+            return await contexto.Combos.AnyAsync(c => c.ComboId > 0);
         }
 
 		private async Task<bool> Insertar(Combos registro)
 		{
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return true;
+			contexto.Combos.Add(registro);
+            return await contexto.SaveChangesAsync() > 0;
         }
 
 		private async Task<bool> Modificar(Combos registro)
 		{
 			await using var contexto = await DbFactory.CreateDbContextAsync();
-			return true;
-		}
+            contexto.Combos.Update(registro);
+            return await contexto.SaveChangesAsync() > 0;
+        }
 
 		public async Task<bool> Eliminar(int id)
 		{
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return true;
+            return await contexto.Combos
+				.Where(c => c.ComboId > 0)
+				.ExecuteDeleteAsync() > 0;
         }
 
-		public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> registro)
+		public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> criterio)
 		{
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return null;
+            return await contexto.Combos
+				.AsNoTracking()
+				.Where(criterio)
+				.ToListAsync();
         }
 
 
 		public async Task<Combos?> Buscar(int id)
 		{
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return null;
+            return await contexto.Combos
+               .AsNoTracking()
+			   .FirstOrDefaultAsync(c => c.ComboId == id);
         }
-		public async Task<bool> Guardar(Combos registro)
+		public async Task<bool> Guardar(Combos combos)
 		{
-			return true;
+			if(!await Existe(combos.ComboId))
+				return await Insertar(combos);
+			else
+				return await Modificar(combos);
 		}
 	}
 }
