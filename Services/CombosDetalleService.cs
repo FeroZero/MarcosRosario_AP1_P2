0dcc16053d146b128f9296f64f2e954b52176ec9
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-25 01:25:02 -0400

    Creacion de CombosDetalleService.

---
+++ Services/CombosDetalleService.cs
@@ -0,0 +1,19 @@
+﻿using MarcosRosario_AP1_P2.DAL;
+using MarcosRosario_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MarcosRosario_AP1_P2.Services
+{
+	public class CombosDetalleService(IDbContextFactory<Contexto> DbFactory)
+	{
+		public async Task<List<CombosDetalle>> Listar(Expression<Func<CombosDetalle, bool>> criterio)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.CombosDetalle
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+		}
+	}
+}
