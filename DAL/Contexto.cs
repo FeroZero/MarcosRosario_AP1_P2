45d0cb85d8c782d04b8971b652763c4940c1c690
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-25 01:36:54 -0400

    modificacion del contexto.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,25 +1,25 @@
 ﻿using MarcosRosario_AP1_P2.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace MarcosRosario_AP1_P2.DAL
+namespace MarcosRosario_AP1_P2.DAL;
+
+public class Contexto : DbContext
 {
-	public class Contexto : DbContext
+	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+	public DbSet<Combos> Combos { get; set; }
+	public DbSet<CombosDetalle> CombosDetalle { get; set; }
+	public DbSet<Articulos> Articulos { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-
-		public DbSet<Combos> Combos { get; set; }
-		public DbSet<CombosDetalle> CombosDetalle { get; set; }
-        public DbSet<Articulos> Articulos { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
-            {
-                new () {ArticuloId = 1, Descripcion = "CPU"},
-                new () {ArticuloId = 2, Descripcion = "GPU"},
-                new () {ArticuloId = 3, Descripcion = "Power"}
-            });
-        }
-    }
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Articulos>().HasData(
+		new List<Articulos>()
+		{
+			new () {ArticuloId = 1, Descripcion = "CPU", Costo = 100, Existencia = 10},
+			new () {ArticuloId = 2, Descripcion = "GPU", Costo = 300, Existencia = 5},
+			new () {ArticuloId = 3, Descripcion = "Power", Costo = 100, Existencia = 25}
+		});
+	}
 }
