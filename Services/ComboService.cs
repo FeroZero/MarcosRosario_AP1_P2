db5180c8009579421340e0a06746a490104fac1d
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-25 16:53:18 -0500

    Cambios en el service.

--- Services/ComboService.cs
+++ Services/ComboService.cs
@@ -21,7 +21,8 @@ public class ComboService(IDbContextFactory<Contexto> DbFactory)
             {
                 articulo.Existencia += item.Cantidad;
             }
-        }
+			await contexto.SaveChangesAsync();
+		}
     }
     private async Task<bool> Existe(int id)
 	{
@@ -40,6 +41,15 @@ public class ComboService(IDbContextFactory<Contexto> DbFactory)
 	private async Task<bool> Modificar(Combos registro)
 	{
 		await using var contexto = await DbFactory.CreateDbContextAsync();
+		var registroCombo = await contexto.Combos
+		.Include(c => c.CombosDetalle)
+		.AsNoTracking()
+		.FirstOrDefaultAsync(c => c.ComboId == registro.ComboId);
+
+		await AfectarArticulo(registroCombo.CombosDetalle.ToArray(), false);
+
+		await AfectarArticulo(registroCombo.CombosDetalle.ToArray(), true);
+
 		contexto.Update(registro);
 		await AfectarArticulo(registro.CombosDetalle.ToArray());
 		var modificado = await contexto.SaveChangesAsync() > 0;
@@ -50,9 +60,15 @@ public class ComboService(IDbContextFactory<Contexto> DbFactory)
 	public async Task<bool> Eliminar(int id)
 	{
 		await using var contexto = await DbFactory.CreateDbContextAsync();
-		return await contexto.Combos
-		.Where(c => c.ComboId > 0)
-		.ExecuteDeleteAsync() > 0;
+		var combos = contexto.Combos.Find(id);
+
+		await AfectarArticulo(combos.CombosDetalle.ToArray(), false);
+
+		contexto.CombosDetalle.RemoveRange(combos.CombosDetalle);
+		contexto.Combos.Remove(combos);
+
+		var cantidad = await contexto.SaveChangesAsync();
+		return cantidad > 0;
 	}
 
 	public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> criterio)
