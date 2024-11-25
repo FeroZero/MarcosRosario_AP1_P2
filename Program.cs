6d6f67e4ef774520d421529fa897cb2428fc7705
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-11-25 01:37:14 -0400

    inyeccion del contexto.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using MarcosRosario_AP1_P2.Components;
 using MarcosRosario_AP1_P2.DAL;
 using MarcosRosario_AP1_P2.Services;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +12,8 @@ var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 builder.Services.AddDbContextFactory<Contexto>(c => c.UseSqlServer(ConStr));
 
 builder.Services.AddScoped<ComboService>();
+builder.Services.AddScoped<ArticuloService>();
+builder.Services.AddScoped<CombosDetalleService>();
 builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
